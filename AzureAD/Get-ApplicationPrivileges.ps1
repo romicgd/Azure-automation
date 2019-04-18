51e02379cf1d2559a401843db48e3607c53293f4
c3.Roman.Guoussev-Do <roman.guoussev-donskoi@ontario.ca>
2019-04-17 20:00:57 -0400

    minor changes and make ps1 instead of txt

--- AzureAD/Get-ApplicationPrivileges.ps1
+++ AzureAD/Get-ApplicationPrivileges.ps1
@@ -1,22 +1,22 @@
- $apps=(get-AzureADApplication -All $true)
+ $apps=(get-AzureADApplication -All $true )
  foreach($app in $apps) {
-	$appid = "'"+$app.appid+"'"; $sp=(get-azureADServicePrincipal -Filter "AppId eq $appid"); 
+	$appid = "'"+$app.appid+"'"; 
 	if($sp.objectid) {
-		foreach($requiredResourceAccess in $sp.requiredresourceaccess) {
+		foreach($requiredResourceAccess in $app.requiredresourceaccess) {
 			$resourceAppId = $requiredResourceAccess.resourceAppId 
 			$resourceappid = "'"+$resourceAppId+"'"; 
 			$resourcesp=(get-azureADServicePrincipal -Filter "AppId eq $resourceappid"); 
 			foreach($approle in $resourcesp.Oauth2Permissions) {
 				foreach($resourceaccess in $requiredResourceAccess.resourceAccess) {
 					if (($approle.id -eq $resourceAccess.id) -and ($resourceAccess.type -eq 'Scope')) {
-						Write-output "$($sp.displayname),$($resourcesp.DisplayName),$($approle.Value)" | out-file ".\delegatedprivs.txt" -append
+						Write-output "$appid,$($app.displayname),$resourceappid,$($resourcesp.DisplayName),$($approle.Value)" | out-file ".\delegatedprivs-all.txt" -append
 					}
 				}
 			}	
 			foreach($approle in $resourcesp.AppRoles) {
 				foreach($resourceaccess in $requiredResourceAccess.resourceAccess) {
 					if (($approle.id -eq $resourceAccess.id) -and ($resourceAccess.type -eq 'Role')) {
-						Write-output "$($sp.displayname),$($resourcesp.DisplayName),$($approle.Value)" | out-file ".\appprivs.txt" -append
+						Write-output "$appid,$($app.displayname),$resourceappid,$($resourcesp.DisplayName),$($approle.Value)" | out-file ".\appprivs-all.txt" -append
 					}
 				}
 			}	
